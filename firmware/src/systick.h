b89ada8ed755e8f17ff68260234b912bf07b2b2a
Robert Fry <inbox@robertfry.xyz>
2026-02-28 11:22:07 +0000

    chore: systick.h documentation

--- firmware/src/systick.h
+++ firmware/src/systick.h
@@ -4,10 +4,22 @@
 
 #include <stdint.h>
 
+/**
+ * Setup the system interrupt clock.
+ */
 void systick_setup(uint32_t ahb_frequency);
 
+/**
+ * Get the number of milliseconds since reset.
+ *
+ * Requires `systick_setup` be called during setup.
+ */
 uint64_t millis(void);
 
+/**
+ * Delay for a number of milliseconds.
+ * Implemented as a busy wait.
+ */
 void delay_ms(uint64_t ms);
 
 #endif /* SYSTICK_H */
