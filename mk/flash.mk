83bfa72483805f0e5962c570d301c5eaa109660f
Robert Fry <inbox@robertfry.xyz>
2026-03-03 16:55:33 +0000

    core: common linker files

--- mk/flash.mk
+++ mk/flash.mk
@@ -1,9 +1,9 @@
 
 STFLASH := st-flash
 
-ifndef FLASH_OFFSET
-  $(error "Variable FLASH_OFFSET is not defined.")
+ifndef FLASH_ORIGIN
+  $(error "Variable FLASH_ORIGIN is not defined.")
 endif
 
 flash: $(BUILD_DIR)/$(BUILD)/$(PLATFORM)-$(BINARY).bin
-	$(STFLASH) write "$<" $(FLASH_OFFSET)
+	$(STFLASH) write "$<" $(FLASH_ORIGIN)
--- mk/platform/nucleo-f446re.mk
+++ mk/platform/nucleo-f446re.mk
@@ -1,4 +1,15 @@
 
+ifndef PROJECT_ROOT
+  $(error "Variable PROJECT_ROOT is not defined.")
+endif
+
+ifndef FLASH_ORIGIN
+  $(error "Variable FLASH_ORIGIN is not defined.")
+endif
+ifndef FLASH_LENGTH
+  $(error "Variable FLASH_LENGTH is not defined.")
+endif
+
 ################################################################################
 # Hardware Configuration (STM32F446RET6)
 
@@ -7,4 +18,7 @@ PLATFORM := nucleo-f446re
 ARCH_FLAGS := -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16
 
 CC_FLAGS += $(ARCH_FLAGS) -DSTM32F4
-LD_FLAGS += $(ARCH_FLAGS) -T$(PLATFORM).ld
+
+LD_FLAGS += $(ARCH_FLAGS) -T$(PROJECT_ROOT)/ld/$(PLATFORM).ld
+LD_FLAGS += -Wl,--defsym=FLASH_ORIGIN=$(FLASH_ORIGIN)
+LD_FLAGS += -Wl,--defsym=FLASH_LENGTH=$(FLASH_LENGTH)
